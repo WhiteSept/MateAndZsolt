4cb6d1918798911e06ddc5ecf9d512fceedaba7f
zsolt.preseka@gmail.com <Gyuszko1004>
2020-03-24 12:13:55 +0100

    alap

--- formulative-frontend/src/app/app-routing.module.ts
+++ formulative-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainInsuranceComponent} from "./components/main-insurance/main-insurance.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: MainInsuranceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
