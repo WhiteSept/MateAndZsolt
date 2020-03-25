73e0cb82fabe0b908d60685150158e7a878f6eb3
Tóth Máté <matetothuk@gmail.com>
2020-03-25 20:23:45 +0100

    formok rendes adattal dolgoznak

--- formulative-frontend/src/app/services/util-repo.service.ts
+++ formulative-frontend/src/app/services/util-repo.service.ts
@@ -1,16 +1,20 @@
-import {Injectable} from '@angular/core';
-import {Subject} from "rxjs";
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Page } from '../enum/page';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilRepoService {
 
   progress = new Subject<Page>();
 
-  numOfInsureds: any;
-  typeOfFrequency: any;
+  numOfInsureds: any [];
+  typeOfFrequency: any [];
+  policyDiscounts: any [];
+  paymentMethods: any [];
+  customerDiscount: any[];
+  campaignDiscount: any[];
 
   constructor() {
     this.numOfInsureds = [
@@ -20,13 +24,43 @@ export class UtilRepoService {
       {name: '4 person', value: 4},
       {name: '5 person', value: 5},
     ];
-      this.typeOfFrequency = [
-        {name: 'Year', value: 1},
-        {name: 'Half', value: 2},
-        {name: 'Quarter', value: 3},
-        {name: 'Month', value: 4}
-      ]
 
+    this.typeOfFrequency = [
+      {name: 'Year', value: 1},
+      {name: 'Half', value: 2},
+      {name: 'Quarter', value: 3},
+      {name: 'Month', value: 4},
+    ];
+
+    this.policyDiscounts = [
+      {name: '1%', value: 0.01},
+      {name: '2%', value: 0.02},
+      {name: '3%', value: 0.03},
+      {name: '4%', value: 0.04},
+      {name: '5%', value: 0.05},
+      {name: '6%', value: 0.06},
+      {name: '7%', value: 0.07},
+      {name: '8%', value: 0.08},
+      {name: '9%', value: 0.09},
+      {name: '10%', value: 0.1},
+    ];
+
+    this.paymentMethods = [
+      {name: 'cash', value: 1},
+      {name: 'bank', value: 1},
+      {name: 'direct bank', value: 1},
+      {name: 'direct same bank', value: 2},
+    ];
+
+    this.campaignDiscount = [
+      {name: 'true', value: 1},
+      {name: 'false', value: 2},
+    ];
+
+    this.customerDiscount = [
+      {name: 'true', value: 1},
+      {name: 'false', value: 2},
+    ];
   }
 
   getNumberOfInsured() {
@@ -35,5 +69,21 @@ export class UtilRepoService {
 
   getTypeOfFrequency() {
     return this.typeOfFrequency;
+  }
+
+  getPolicyDiscounts() {
+    return this.policyDiscounts;
+  }
+
+  getPaymentMethods() {
+    return this.paymentMethods;
+  }
+
+  getCustomerDiscount() {
+    return this.customerDiscount;
+  }
+
+  getCampaignDiscount(){
+    return this.campaignDiscount;
   }
 }
